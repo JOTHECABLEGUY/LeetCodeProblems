63cd8b5edd172cf17aa12532fc694ca18b23762e
JOTHECABLEGUY <jzdube99@gmail.com>
2024-08-22 16:41:40 -0400

    Practicing python and c++

---
+++ Python/fib.py
@@ -0,0 +1,15 @@
+from time import perf_counter
+import sys
+def fib(num:int):
+    if num == 0:
+        return 0
+    if num == 1:
+        return 1
+    return fib(num-1) + fib(num-2)
+def main():
+    target_number_index = sys.argv[1] if len(sys.argv[1:]) else 50
+    start = perf_counter()
+    print(fib(50))
+    print(f"time taken: {perf_counter()-start:.4f} seconds")
+if __name__ == "__main__":
+    main()
