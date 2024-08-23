8aeb009661dd9ca6e0cedd13a4acb069fb040d8d
JOTHECABLEGUY <jzdube99@gmail.com>
2024-08-23 00:10:35 -0400

    added caching as well as minor input logic changes

--- Python/fib.py
+++ Python/fib.py
@@ -1,11 +1,25 @@
 from time import perf_counter
 import sys
-def fib(num:int):
-    return 0 if num <= 0 else fib(num-1) + fib(num-2)
+from functools import lru_cache
+@lru_cache()
+def fib(num:int): # 1, 1, 2, 3
+    """
+    Compute the Fibonacci number at a given index using memoization.
+
+    This function returns the Fibonacci number corresponding to the specified index. 
+    It utilizes caching to optimize performance, allowing for efficient computation even for larger indices.
+
+    Args:
+        num (int): The index of the Fibonacci number to compute, where the sequence starts with fib(0) = 0 and fib(1) = 1.
+
+    Returns:
+        int: The Fibonacci number at the specified index.
+    """
+    return num if num <= 1 else fib(num-1) + fib(num-2)
 def main():
-    target_number_index = sys.argv[1] if len(sys.argv[1:]) else 50
+    target_number_index = int(sys.argv[1]) if len(sys.argv) > 1 else 50
     start = perf_counter()
-    print(fib(50))
+    print(fib(target_number_index))
     print(f"time taken: {perf_counter()-start:.4f} seconds")
 if __name__ == "__main__":
     main()
