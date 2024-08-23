45505a4c0c3d81d610d8b8322aed51abfcbb927c
JOTHECABLEGUY <jzdube99@gmail.com>
2024-08-23 14:52:22 -0400

    updated calculator to have more comprehensive checks before evaluating, as well as during evaluation

--- Python/calculator.py
+++ Python/calculator.py
@@ -1,5 +1,6 @@
 import re
 import sys
+
 class Calculator(object):
     def __init__(self):
         self.expression = ""
@@ -7,11 +8,16 @@ class Calculator(object):
         self.valid = False
     def get_result(self):
         self.is_expression_valid()
-        return eval(self.expression) if self.valid else sys.maxsize
+        try:
+            return eval(self.expression)
+        except Exception as e:
+            print(f"Invalid expression: either the expression results in a denominator equal to 0 or its syntax is incorrect: {self.expression}")
+            return sys.maxsize
     def is_expression_valid(self):
-        if re.match(r"[\(\)\+\-/\*!0-9]+", self.expression) is not None:
-            self.valid = True
-        self.valid = False
+        search_for_valid_math_chars = re.compile(r"[\(\)+\-/\*\d]+$").match(self.expression)
+        search_for_div_by_0 = re.compile(r"/\(?(\([+\-\d]+\))*([\*\d])*0").search(self.expression)
+        
+        self.valid = search_for_valid_math_chars is not None and search_for_div_by_0 is None
         
 def main():
     calculator = Calculator()
