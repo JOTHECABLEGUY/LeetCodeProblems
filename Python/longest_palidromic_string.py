2a86dae0930181546ec908795459004e6475c7e1
JOTHECABLEGUY <jzdube99@gmail.com>
2024-08-29 10:03:18 -0400

    revised O(n^2) approach to be more readable

--- Python/longest_palidromic_string.py
+++ Python/longest_palidromic_string.py
@@ -46,41 +46,44 @@ class Solution:
                     max_len = seq_len
         return max_len_seq
     
-    # complexity: O(n^2)
+    # helper function to expand around a given center. 
+    #   If left == right, looking for odd length substrings,
+    #   If left != right, looking for even length substrings
+    def expand(self, left:int, right:int, s:str)-> int:
+        s_len = len(s)
+        while left >= 0 and right < s_len and s[left] == s[right]:
+            left -= 1
+            right += 1
+        # the condition for ending the while loop is the pointers exceed the bounds of the string 
+        #   or left does not equal right, meaning the pointers are currently not correct. 
+        #   We account for this by subtracting 2 from the original distance (orig dist = right-left+1)
+        return right-left-1
+    # complexity: O(n^2), expand around centers approach
     def longestPalindrome_2(self, s:str) -> str:
-        max_len:int = 0
-        max_sub:str = ""
+        max_dist:int = 0
         max_start = 0
-        if len(s) == 1: 
+        s_len = len(s)
+        
+        # quit early if a palindromic input string is supplied or the length is 2
+        if s == s[::-1]:
             return s
-
-        if len(s) == 2:
-            return s[0] if s != s[::-1] else s
-        # even length palidromes
-        for i in range(len(s)-1):
-            left = i
-            right = i+1
-            while(right < len(s) and left >= 0 and s[left] == s[right]):
-                right += 1
-                left -= 1
-            new_len = right-left+1
-            if new_len > max_len:
-                max_len = new_len
-                max_start = left
-
-        # odd length palindromes
-        for i in range(len(s)-2):
-            left = i
-            right = i+2
-            while(right < len(s) and left >= 0 and s[left] == s[right]):
-                right += 1
-                left -= 1
-            new_len = right-left+1
-            if new_len > max_len:
-                max_len = new_len
-                max_start = left
-        return s[max_start+1:max_start+max_len]
-            
+        if s_len == 2:
+            return s[0]
+        
+        # the last char in the string cannot be the center of the longest palindrome 
+        #   (the second to last would capture the last char in its own palindrome)
+        for center in range(s_len-1):
+            odd_dist = self.expand(center, center, s)
+            if odd_dist > max_dist:
+                radius = odd_dist//2
+                max_start = center - radius
+                max_dist = odd_dist
+            even_dist = self.expand(center, center+1, s)
+            if even_dist > max_dist:
+                radius = even_dist //2 - 1
+                max_start = center-radius
+                max_dist = even_dist
+        return s[max_start: max_start + max_dist]
                     
 
 
