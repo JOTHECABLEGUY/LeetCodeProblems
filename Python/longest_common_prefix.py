1fd2cdfe47dfe7fa6451da4c38e084c3e41f0265
JOTHECABLEGUY <jzdube99@gmail.com>
2024-08-31 16:02:25 -0400

    added comments and simplified the solution

--- Python/longest_common_prefix.py
+++ Python/longest_common_prefix.py
@@ -26,17 +26,36 @@ import pytest
 from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        min_str_len = min(len(s) for s in strs)
-        c = [""]*min_str_len
+        """
+            Finds the longest common prefix among a list of strings.
+
+            This function takes a list of strings and returns the longest common prefix shared 
+            by all the strings in the list. If there is no common prefix, it returns an empty string.
+
+            Args:
+                strs (List[str]): A list of strings to evaluate for the longest common prefix.
+
+            Returns:
+                str: The longest common prefix found in the list of strings, or an empty string 
+                if no common prefix exists.
+        """
+        # if smallest string is empty, return nothing
+        if not (min_str_len := min(len(s) for s in strs)):
+            return ""
+        # for each index in the smallest string, build a set with each character at the same position
+        #   if any characters are different, the set will have a length greater than 1, so decrement i 
+        #   and break the loop, otherwise continue
         for i in range(min_str_len):
-            char = strs[0][i]
-            chars = [s[i] for s in strs]
-            if any(char != c for c in chars):
+            if len({s[i] for s in strs})-1:
+                i-=1
                 break
-            c[i] = char
-        return "".join(c)
+        
+        # once the loop breaks, we know that the longest common prefix is from indices 0 to i+1 (when the loop
+        #   ends, either i was decremented or i = the last index of the string), so i needs to be 1 more in 
+        #   order for the slicing to capture all the characters (slicing stops 1 before the supplied index).
+        return strs[0][:i+1]
     def test(self):
-        return self.longestCommonPrefix(["dog", "dogbone", "debt"])
+        return self.longestCommonPrefix(["dog", "dog", "dog"])
 @pytest.mark.parametrize(
         "strings, expected", 
     [
