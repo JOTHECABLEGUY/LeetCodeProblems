fb5f13a566637b4cc31352d45690cefcccdcb504
JOTHECABLEGUY <jzdube99@gmail.com>
2024-09-04 11:31:09 -0400

    streamlined testing and linkedlist logic

--- Python/remove_nth_node_from_list.py
+++ Python/remove_nth_node_from_list.py
@@ -23,7 +23,6 @@ The number of nodes in the list is sz.
 1 <= n <= sz
 
 Follow up: Could you do this in one pass?"""
-from operator import index
 import pytest
 from typing import Optional
 # Definition for singly-linked list.
@@ -34,10 +33,7 @@ class ListNode:
 class Solution:
     def test(self):
         nums = [1]
-        dummy = ListNode(val = nums[-1], next = None)
-        for n in nums[-2::-1]:
-            dummy = ListNode(val = n, next = dummy)
-        return self.removeNthFromEnd(dummy, 1)
+        return self.removeNthFromEnd(list_to_linkedlist(nums), 1)
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         """
             Removes the nth node from the end of a linked list.
@@ -52,31 +48,80 @@ class Solution:
             Returns:
                 Optional[ListNode]: The head of the modified linked list after removing the nth node.
         """
-        if n == 0 or head is None:
-            return head
-        nodes = []
-        while head is not None:
-            nodes.append(head)
-            head = head.next
-        index_to_remove = len(nodes)-n
-        if index_to_remove > len(nodes) or index_to_remove < 0:
-            return nodes[0]
+        first_run = second_run = head
+        for _ in range(n):
+            if first_run is None:
+                return head
+            first_run = first_run.next
+        if first_run is None:
+            return head.next
+        while first_run.next is not None:
+            first_run = first_run.next
+            second_run = second_run.next
+        second_run.next = second_run.next.next
+        return head
+    
+def linkedlist_to_list(node):
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    return result
+def list_to_linkedlist(lst):
+    head = ListNode(lst[0])
+    current = head
+    for value in lst[1:]:
+        current.next = ListNode(value)
+        current = current.next
+    return head
+@pytest.mark.parametrize(
+    "head, n, expected",
+    [
+        ([1, 2, 3, 4, 5], 2, [1, 2, 3, 5]),  # happy path
+        ([1], 1, []),  # single element list
+        ([1, 2], 1, [1]),  # two elements, remove last
+        ([1, 2], 2, [2]),  # two elements, remove first
+        ([1, 2, 3], 3, [2, 3]),  # remove head
+        ([1, 2, 3], 1, [1, 2]),  # remove tail
+    ],
+    ids=[
+        "remove 2nd from end in [1,2,3,4,5]",
+        "remove 1st from end in [1]",
+        "remove 1st from end in [1,2]",
+        "remove 2nd from end in [1,2]",
+        "remove 3rd from end in [1,2,3]",
+        "remove 1st from end in [1,2,3]",
+    ]
+)
+def test_removeNthFromEnd(head, n, expected):
+    # Act
+    head_node = list_to_linkedlist(head)
+    result_node = Solution().removeNthFromEnd(head_node, n)
+    result_list = linkedlist_to_list(result_node)
 
-        # unlink from node to remove
-        nodes[index_to_remove].next = None
+    # Assert
+    assert result_list == expected
 
-        if index_to_remove == 0 and len(nodes) > 1:
-            return nodes[1]
-        if index_to_remove <= 0:
-            return None
-        nodes[index_to_remove-1].next = nodes[index_to_remove+1]if index_to_remove < len(nodes)-1 else None
-        return nodes[0]
+@pytest.mark.parametrize(
+    "head, n, expected",
+    [
+        ([], 1, []),  # empty list
+        ([1], 2, [1]),  # n greater than list length
+    ],
+    ids=[
+        "empty list",
+        "n greater than list length",
+    ]
+)
+def test_removeNthFromEnd_edge_cases(head, n, expected):
+    # Act
+    head_node = list_to_linkedlist(head) if head else None
+    result_node = Solution().removeNthFromEnd(head_node, n)
+    result_list = linkedlist_to_list(result_node)
+
+    # Assert
+    assert result_list == expected
+    
 if __name__ == "__main__":
     res = Solution().test()
-    while res is not None:
-        if res.next is not None:
-            print(res.val, '->', res.next.val)
-        else:
-            print(res.val)
-        res = res.next
-        
+    print(linkedlist_to_list(res))
