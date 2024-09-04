9086763ab9682fb12bc7ece984060e7e1ee3a875
JOTHECABLEGUY <jzdube99@gmail.com>
2024-09-04 10:40:42 -0400

    first attempt

---
+++ Python/remove_nth_node_from_list.py
@@ -0,0 +1,73 @@
+"""19. Remove Nth Node From End of List
+Medium
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
+Example 2:
+
+Input: head = [1], n = 1
+Output: []
+Example 3:
+
+Input: head = [1,2], n = 1
+Output: [1]
+
+Constraints:
+
+The number of nodes in the list is sz.
+1 <= sz <= 30
+0 <= Node.val <= 100
+1 <= n <= sz
+
+Follow up: Could you do this in one pass?"""
+import pytest
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def test(self):
+        nums = [1, 2]
+        dummy = ListNode(val = nums[-1], next = None)
+        for n in nums[-2::-1]:
+            dummy = ListNode(val = n, next = dummy)
+        return self.removeNthFromEnd(dummy, 1)
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        """
+            Removes the nth node from the end of a linked list.
+
+            This function takes the head of a linked list and an integer n, and removes the 
+            nth node from the end of the list. It returns the head of the modified list.
+
+            Args:
+                head (Optional[ListNode]): The head of the linked list.
+                n (int): The position of the node to remove from the end of the list.
+
+            Returns:
+                Optional[ListNode]: The head of the modified linked list after removing the nth node.
+        """
+        if n == 0 or head is None:
+            return head
+        nodes = []
+        while head is not None:
+            nodes.append(head)
+            head = head.next
+        index_to_remove = len(nodes)-n
+        if index_to_remove > len(nodes) or index_to_remove < 0:
+            return nodes[0]
+        nodes[-n].next = None
+        nodes.pop(-n)
+        if not len(nodes):
+            return None
+        if n == 1:
+            nodes[-n].next = None
+        elif n<= len(nodes):
+            nodes[-n].next = nodes[-n+1]
+        return nodes[0]
+if __name__ == "__main__":
+    print(Solution().test())
