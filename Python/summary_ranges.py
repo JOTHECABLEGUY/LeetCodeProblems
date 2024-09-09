be9582662f39ab0e79d542f6e6ce40d1708352a7
JOTHECABLEGUY <jzdube99@gmail.com>
2024-09-09 14:39:09 -0400

    small logic modification, added comments and documentation

--- Python/summary_ranges.py
+++ Python/summary_ranges.py
@@ -45,23 +45,58 @@ class Solution:
         return self.summaryRanges([1, 2, 3, 4, 6])
     
     def summaryRanges(self, nums: List[int]) -> List[str]:
+        """
+            Summarize a list of integers into a list of ranges.
+
+            This function takes a sorted list of integers and groups consecutive numbers into ranges. 
+            It returns a list of strings representing these ranges, where a range is formatted as 
+            "start->end" for consecutive numbers, and single numbers are represented as themselves.
+
+            Args:
+                nums (List[int]): A list of sorted integers.
+
+            Returns:
+                List[str]: A list of strings representing the summarized ranges.
+
+        """
+        
+        # return in empty list is provided
         if not nums:
             return []
-        # l, r = 0,0
+        
+        # list to hold range strings
         res = []
-        # range_map = {}
+        
+        # list to hold working ranges of numbers
         ranges = [[nums[0]]]
+        
+        # for each number after the first
         for i in range(1, len(nums)):
+            
+            # get the most recent range
             curr_range = ranges[-1]
-            if curr_range and nums[i] - 1 == curr_range[-1]:
+            
+            # check if the last number is 1 less than the current number (meaning this number is consecutive).
+            #   If it is, add it to the range
+            if nums[i] - 1 == curr_range[-1]:
                 curr_range.append(nums[i])
+                
+            # otherwise, make a new range in the ranges list
             else:
                 ranges.append([nums[i]])
+        
+        # for each range, make a string representing the list
         for r in ranges:
+            
+            # if there is one element, only put the number as a string
             if len(r) == 1:
                 res.append(str(r[0]))
+            
+            # otherwise put the first and last elements of the list with an arrow between them
             else:
                 res.append(f"{r[0]}->{r[-1]}")
+        
+        # return the list of ranges expressed as strings
         return res
 
 @pytest.mark.parametrize(
