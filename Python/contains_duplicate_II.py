4763e45d191a85ad5c6d7895262899a0032f51d1
JOTHECABLEGUY <jzdube99@gmail.com>
2024-09-09 14:07:14 -0400

    second attempt

--- Python/contains_duplicate_II.py
+++ Python/contains_duplicate_II.py
@@ -33,20 +33,41 @@ class Solution:
         return self.containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 3)
     
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        """
+            Check if there are two indices in the list such that their values are equal and their indices 
+            are at most k apart.
+
+            This function determines if any value appears at least twice in the list with their indices 
+            differing by no more than k. It returns True if such a pair exists, otherwise it returns False.
+
+            Args:
+                nums (List[int]): A list of integers to check for nearby duplicates.
+                k (int): The maximum allowed index difference between duplicate values.
+
+            Returns:
+                bool: True if there are duplicates within the specified index range, otherwise False.
+        """
+        
+        # check if inputs are valid, return False if they are invalid
         if not nums or len(nums) == 1 or k <= 0:
             return False
+        
+        # if there are k or fewer elements in the list, check the whole list for duplicates
         if k >= len(nums):
             return len(set(nums)) != len(nums)
-        r = k +1
-        prev_seq = nums[:r]
-        if len(set(prev_seq)) != k+1:
-            return True
-        while r < len(nums):
-            prev_seq.pop(0)
-            prev_seq.append(nums[r])
-            if len(set(prev_seq)) != k+1:
+        
+        # table to store indices of the last occurrence of number
+        table = {}
+        
+        # for each number, check if it has been seen. if it has been seen, then check the difference
+        #   between the current index and the last index where the current number was seen. If the difference
+        #   if below or equal to k, then we have found a duplicate, otherwise update the last seen index 
+        for i, num in enumerate(nums):
+            if num in table and i - table[num] <= k:
                 return True
-            r+=1
+            table[num] = i
+        
+        # if duplicates were no found, return False
         return False
 
 @pytest.mark.parametrize(
