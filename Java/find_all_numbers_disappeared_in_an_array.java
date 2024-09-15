ed51690ba39ad2e799565897f05e0f9292eb15ed
JOTHECABLEGUY <jzdube99@gmail.com>
2024-09-15 13:37:05 -0400

    added comments, with some minor logic changes

--- Java/find_all_numbers_disappeared_in_an_array.java
+++ Java/find_all_numbers_disappeared_in_an_array.java
@@ -23,14 +23,34 @@ Follow up: Could you do it without extra space and in O(n) runtime? You may assu
 import java.util.*;
 
 class Solution {
+
+    /**
+     * Method to determine which numbers are missing in the range [1, n] inclusive for an array of size n
+     * @param nums (int[]): nums to look through to determine which are missing
+     * @return List<Integer>: list of numbers that are missing from the input array
+     */
     public List<Integer> findDisappearedNumbers(int[] nums) {
+
+        // length of array, also stopping point of the numbers expected in the input array
         int n = nums.length;
-        boolean[] present = new boolean[n];
-        for (int i = 0; i < n; i++){ present[nums[i]-1] = true; }
+
+        // array to track which elements are present in the input array
+        boolean[] present = new boolean[n+1];
+
+        // mark each element in the input array as present
+        for (int i : nums){ present[i] = true; }
+
+        // list to hold the elements that are missing
         List<Integer> l = new ArrayList<>();
-        for(int i = 0; i < n; i++){ if (!present[i]) l.add(i+1); }
+
+        // add each missing element to the list (start at 1 and got to n, check if value in 
+        //  present array is false before appending)
+        for(int i = 1; i <= n; i++){ if (!present[i]) l.add(i); }
+
+        // return the list of missing elements
         return l;
     }
+    
     public static void main(String[] args){
         int[] nums = {1, 3, 3, 4, 4};
         Solution obj = new Solution();
