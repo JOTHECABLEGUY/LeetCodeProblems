37e775c0c7ce23cbad5141ca3d260613028eebfc
JOTHECABLEGUY <jzdube99@gmail.com>
2024-09-15 22:42:11 -0400

    added comments

--- Java/island_perimeter.java
+++ Java/island_perimeter.java
@@ -33,22 +33,51 @@ There is exactly one island in grid.
  */
 
 class Solution {
+
+    /**
+     * Method to find the perimeter of square cells of land arranged within a grid
+     * @param grid (int[][]): matrix of dimensions r x c where r is the number of rows, c is the number of columns, and each
+     *                          cell represents a 1x1 unit of land if the value is 1, water if 0
+     * @return int: perimeter of the landmass present in the grid
+     */
     public int islandPerimeter(int[][] grid) {
+
+        // init perimeter, num of rows, and num of columns
         int perim = 0;
         int rows = grid.length;
         int cols = grid[0].length;
+
+        // each land cell has at most 4 sides that contribute to the entire landmass's perimeter
         int exposed_sides = 4;
+
+        // traverse the grid
         for (int i = 0; i < rows; i++){
             for (int j = 0; j < cols; j++){
+
+                // continue if the current cell is water
                 if (grid[i][j] == 0) continue;
+
+                // remove an exposed side if the cell to the north is land
                 if (i != 0 && grid[i-1][j] == 1) exposed_sides--;
+
+                // remove an exposed side if the cell to the south is land
                 if (i < rows-1 && grid[i+1][j] == 1) exposed_sides--;
+
+                // remove an exposed side if the cell to the west is land
                 if (j != 0 && grid[i][j-1] == 1) exposed_sides--;
+
+                // remove an exposed side if the cell to the east is land
                 if (j < cols-1 && grid[i][j+1] == 1) exposed_sides--;
+
+                // add the number of exposed sides for the current cell to the perimeter
                 perim += exposed_sides;
+
+                // reset exposed sides for the next land cell
                 exposed_sides = 4;
             }
         }
+
+        // return the resulting perimeter
         return perim;
     }
     public static void main(String[] args) {
