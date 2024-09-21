c102d99de354725ca9c12d5a598eea17f2ae3c8f
JOTHECABLEGUY <jzdube99@gmail.com>
2024-09-21 13:45:41 -0400

    added an older version of the method for readability

--- Python/best_time_to_buy_and_sell_stock_ii.py
+++ Python/best_time_to_buy_and_sell_stock_ii.py
@@ -41,6 +41,14 @@ class Solution:
     
     def maxProfit(self, prices: List[int]) -> int:
         return sum(prices[i] - prices[i-1] for i in range(1, len(prices)) if prices[i] > prices[i-1]) if len(prices) > 1 else 0
+    
+    def maxProfit2(self, prices: List[int]) -> int:
+        if len(prices) < 2: return 0
+        max_profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i-1]:
+                max_profit += prices[i] - prices[i-1]
+        return max_profit
 
 @pytest.mark.parametrize("prices, expected, _id", [
     # Happy path tests
