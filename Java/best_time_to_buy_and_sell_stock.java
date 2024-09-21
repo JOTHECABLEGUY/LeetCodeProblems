9e40ae576101438d089d3a3505921c8d04447e47
JOTHECABLEGUY <jzdube99@gmail.com>
2024-09-21 09:38:35 -0400

    added comments

--- Java/best_time_to_buy_and_sell_stock.java
+++ Java/best_time_to_buy_and_sell_stock.java
@@ -28,15 +28,33 @@ Constraints:
 
 
 class best_time_to_buy_and_sell_stock {
+
+    /**
+     * Method to determine the maximum profit possible from buying a stock and selling it one 
+     *  or more days later given the prices ordered by days
+     * @param prices (int[]): prices ordered by time
+     * @return int: maximum profit possible if a stock is bought one day and sold at a later date
+     */
     public int maxProfit(int[] prices) {
+
+        // return early if no profit is possible
         if (prices.length < 2) return 0;
+
+        // keep track of the maximum profit as well as the cheapest price seen so far
         int max_diff = 0;
         int cheapest_so_far = prices[0];
 
+        // loop through the other prices
         for (int i = 1; i < prices.length; i++){
+            
+            // update the cheapest seen so far
             if (prices[i] < cheapest_so_far) {cheapest_so_far = prices[i]; continue;}
+
+            // update the max profit so far
             if (prices[i] - cheapest_so_far > max_diff) max_diff = prices[i] - cheapest_so_far;
         }
+
+        // return the maximum profit possible
         return max_diff;
     }
 
@@ -44,7 +62,7 @@ class best_time_to_buy_and_sell_stock {
         int[] prices1 = {7, 1, 5, 3, 6, 4};
         int[] prices2 = {7, 6, 4, 3, 1};
         best_time_to_buy_and_sell_stock obj = new best_time_to_buy_and_sell_stock();
-        System.out.println(obj.maxProfit(prices1));
-        System.out.println(obj.maxProfit(prices2));
+        System.out.println(obj.maxProfit(prices1)); // outputs 5: buy at 1 and sell at 6
+        System.out.println(obj.maxProfit(prices2)); // outputs 0, prices are never increase
     }
 }
