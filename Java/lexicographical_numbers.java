1c9cef0c89af02440503727b6a0bbe31a8e3f122
JOTHECABLEGUY <jzdube99@gmail.com>
2024-09-21 12:16:40 -0400

    added comments

--- Java/lexicographical_numbers.java
+++ Java/lexicographical_numbers.java
@@ -21,23 +21,54 @@ Constraints:
 import java.util.*;
 
 class Solution {
+
+
+    /**
+     * Method to sort all the numbers in the range [1, n] in lexicographical order given an integer n.
+     * @param n (int): limit for numbers to sort (i.e numbers that will be sorted are [1, n] inclusive)
+     * @return List<Integer>: List of integers in range [1, n] sorted in lexicographical order
+     */
     public List<Integer> lexicalOrder(int n) {
+
+        // declare return list
         List<Integer> lexOrder = new ArrayList<>();
+
+        // return early if the input number is invalid
         if (n < 1) return lexOrder;
 
+        // for each single digit integer base, dig deeper on it to fill the list in the correct order
         for (int i = 1; i < 10; i++) 
             { dfs(lexOrder, n, i);}
 
+        // return the assembled list
         return lexOrder;
-        
     }
 
+    /**
+     * Recursive method to fill the array in lex order given a 
+     *  current element to consider and expand upon
+     * @param arr (int[]): array to add numbers to
+     * @param n (int): limit of magnitude for numbers to add
+     * @param current (int): current base to consider
+     */
     private void dfs(List<Integer> arr, int n, int current){
+
+        // stop digging if the current number is above the limit
         if (current > n) return;
+
+        // add the current number to the list
         arr.add(current);
+
+        // for each number that can be added to the current base
         for (int i = 0; i < 10; i++){
+
+            // create a number that expands on the current base
             int new_num = current * 10 + i;
+
+            // stop this branch if the new number is above the limit
             if (new_num > n) return;
+
+            // expand on the new number in the list
             dfs(arr, n, new_num);
         }
     }
