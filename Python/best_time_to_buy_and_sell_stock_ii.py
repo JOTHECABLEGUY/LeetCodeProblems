6bc8035ae92d4b3da422e92d8d179f2b19a13163
JOTHECABLEGUY <jzdube99@gmail.com>
2024-09-21 13:42:27 -0400

    condensed to for-loop

--- Python/best_time_to_buy_and_sell_stock_ii.py
+++ Python/best_time_to_buy_and_sell_stock_ii.py
@@ -41,10 +41,11 @@ class Solution:
     
     def maxProfit(self, prices: List[int]) -> int:
         if len(prices) < 2: return 0
-        max_profit = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[i-1]:
-                max_profit += prices[i] - prices[i-1]
+        # max_profit = 0
+        # for i in range(1, len(prices)):
+        #     if prices[i] > prices[i-1]:
+        #         max_profit += prices[i] - prices[i-1]
+        max_profit = sum(prices[i] - prices[i-1] for i in range(1, len(prices)) if prices[i] > prices[i-1])
         return max_profit
 
 @pytest.mark.parametrize("prices, expected, _id", [
