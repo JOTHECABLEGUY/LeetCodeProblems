627be8a7611642f7fbff065241c4f7e3f9b86d6a
JOTHECABLEGUY <jzdube99@gmail.com>
2024-10-01 09:51:01 -0400

    streamlined final loop to take half the time by adding additional check before looping

--- Java/check_if_array_pairs_are_divisible_by_k.java
+++ Java/check_if_array_pairs_are_divisible_by_k.java
@@ -35,8 +35,8 @@ class Solution {
     public boolean canArrange(int[] arr, int k) {
         int[] freq = new int[k];
         for (int i : arr) freq[(i%k + k)%k]++;
-        if (freq[0]%2 != 0) return false;
-        for (int i = 1; i < freq.length; i++){
+        if (freq[0]%2 != 0 || (k%2 == 0 && freq[k/2]%2 != 0)) return false;
+        for (int i = 1; i < k/2+1; i++){
             if (freq[i] != freq[k-i]) return false;
         }
         return true;
